1b819b44aaf3ad3606a7db4021d97c5deee4e42d
zceiekr <emile@kratiroff.com>
2016-12-03 00:14:22 +0000

    clean

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(
-	name='Tintin',
-    version='0.0.1',
-    description='Messenger bot',
-    author='Emile Kratiroff',
-    author_email='emile@kratiroff.com',
-    install_requires=['Flask>=0.10.1'],
+	name='Chatbot',
+	version='0.0.1',
+	description='Messenger bot',
+	author='Emile Kratiroff',
+	author_email='emile@kratiroff.com',
+	install_requires=['Flask>=0.10.1'],
 )
